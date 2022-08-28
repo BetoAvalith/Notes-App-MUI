373a8bc22031a335aef64a0248f394ad4534e6cb
Beto <robledo.mdp@gmail.com>
2022-08-28 11:42:52 -0300

    Clase 27

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import { Link as RouterLink } from "react-router-dom";
+
+import { AuthLayout } from "../layout/AuthLayout";
+
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+
+import { Google } from "@mui/icons-material";
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <AuthLayout title='Crear cuenta'>    
+        <form>
+        <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre Completo"
+              type="text"
+              placeholder="Nombre Completo"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@gmail.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid>
+          <Grid container spacing={1} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <Button variant="contained" fullWidth>
+                <Typography sx={{ ml: 1 }}>Crear cuenta</Typography>
+              </Button>
+            </Grid>            
+          </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Ya tengo una cuenta
+            </Link>
+          </Grid>
+        </form>
+      </AuthLayout>
+  );
+};
--- src/notes/pages/NotesPage.jsx
+++ src/notes/pages/NotesPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Typography } from '@mui/material'
+import { NoteAddOutlined } from '@mui/icons-material'
+import { NotesLayout } from '../layout/NotesLayout'
 
 export const NotesPage = () => {
   return (
-    <div>NotesPage</div>
+    <NotesLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis vero quisquam nesciunt iste quod fugit quasi hic unde aut, praesentium sint non! Nulla soluta sint repellat? Atque vero tempora a.
+        Quibusdam quis voluptas a! Ratione, magnam esse tempora earum provident inventore explicabo quae amet atque enim nihil vitae. Maxime quidem itaque ducimus placeat sed officiis illo est dolore eligendi ab.
+        Ipsam obcaecati magni hic repellat iste ab similique ullam recusandae corrupti? Sunt fugit vero, necessitatibus, perferendis quas, quidem velit ipsam nostrum quaerat ut ea accusantium cum. Modi, iste ratione. Nam?
+      </Typography>
+    </NotesLayout>
   )
 }
