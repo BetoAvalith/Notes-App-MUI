c83367b806ad9e87866e2e3d5956812ee7b242d0
Beto <robledo.mdp@gmail.com>
2022-08-29 16:04:54 -0300

    Clase 28

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -5,17 +5,43 @@ import { AuthLayout } from "../layout/AuthLayout";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 
 import { Google } from "@mui/icons-material";
+import { useForm } from "../../hooks";
+import { useDispatch } from "react-redux";
+import { checkingAuthentication, startGoogleSignIn } from "../../store/auth/thunks";
 
 export const LoginPage = () => {
+
+  const dispatch = useDispatch();
+
+  const { email, password, onInputChange } = useForm({
+    email: 'skillfactory@gmail.com',
+    password: '123456'
+  })
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log({email, password});
+    dispatch(checkingAuthentication());
+  }
+
+  const onGoogleSignIn = ()=>{
+    console.log('onGoogleSignIn');
+
+    dispatch(startGoogleSignIn());
+  }
+
   return (
     <AuthLayout title='Login'>    
-        <form>
+        <form onSubmit={onSubmit}>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               label="Correo"
               type="email"
               placeholder="correo@gmail.com"
               fullWidth
+              name='email'
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -24,16 +50,27 @@ export const LoginPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid container spacing={1} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sx={{ mt: 2 }}>
-              <Button variant="contained" fullWidth>
+              <Button 
+                variant="contained" 
+                fullWidth
+                type='submit'
+                >
                 <Typography sx={{ ml: 1 }}>Login</Typography>
               </Button>
             </Grid>
             <Grid item xs={12} sx={{ mt: 2 }}>
-              <Button variant="contained" fullWidth>
+              <Button
+                variant="contained" 
+                fullWidth
+                onClick={onGoogleSignIn}
+              >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
--- src/notes/pages/NotesPage.jsx
+++ src/notes/pages/NotesPage.jsx
@@ -1,16 +1,30 @@
+import { AddOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import React from 'react'
-import { Typography } from '@mui/material'
-import { NoteAddOutlined } from '@mui/icons-material'
-import { NotesLayout } from '../layout/NotesLayout'
+
+import { NotesLayout } from '../layout/NotesLayout';
+import { NoteView, NothingSelectedView } from '../views';
+
 
 export const NotesPage = () => {
   return (
-    <NotesLayout>
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis vero quisquam nesciunt iste quod fugit quasi hic unde aut, praesentium sint non! Nulla soluta sint repellat? Atque vero tempora a.
-        Quibusdam quis voluptas a! Ratione, magnam esse tempora earum provident inventore explicabo quae amet atque enim nihil vitae. Maxime quidem itaque ducimus placeat sed officiis illo est dolore eligendi ab.
-        Ipsam obcaecati magni hic repellat iste ab similique ullam recusandae corrupti? Sunt fugit vero, necessitatibus, perferendis quas, quidem velit ipsam nostrum quaerat ut ea accusantium cum. Modi, iste ratione. Nam?
-      </Typography>
+    <NotesLayout>      
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: '#fff',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />    
+      </IconButton>
     </NotesLayout>
   )
 }
