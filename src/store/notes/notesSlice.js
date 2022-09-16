81bc7baa6378725de5d5bf4cf74e77432883ca63
Beto <robledo.mdp@gmail.com>
2022-09-16 16:04:14 -0300

    Clase 34

--- src/store/notes/notesSlice.js
+++ src/store/notes/notesSlice.js
@@ -52,7 +52,11 @@ export const noteSlice = createSlice({
       state.notes = [];
       state.activeNote = null;
     },
-    deleteNoteById: (state) => {},
+    deleteNoteById: (state, {payload}) => {
+      state.notes = state.notes.filter(note => note.id !== payload);
+      state.activeNote = null;
+      state.isSaving = false;
+    },
   },
 });
 
